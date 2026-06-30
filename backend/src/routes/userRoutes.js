403966e315c370b5b67b91661f54367deb142b46
akshith <2303A51959@sru.edu.in>
2026-06-30 12:11:26 +0530

    packages installed

--- backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProfile, updateProfile, addOrUpdateVehicle } = require('../controllers/userController');
-const { protect } = require('../middleware/auth');
-router.get('/profile', protect, getProfile);
-router.put('/profile', protect, updateProfile);
-router.post('/vehicle', protect, addOrUpdateVehicle);
-module.exports = router;
