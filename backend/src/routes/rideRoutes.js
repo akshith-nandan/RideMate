5ab58d9dc0d253b4a47bc34adb21550d89c9a58e
akshith <2303A51959@sru.edu.in>
2026-06-30 12:11:26 +0530

    packages installed

--- backend/src/routes/rideRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createRide, getRides, getRideById } = require('../controllers/rideController');
-const { protect } = require('../middleware/auth');
-router.post('/create', protect, createRide);
-router.get('/', protect, getRides);
-router.get('/:id', protect, getRideById);
-module.exports = router;
